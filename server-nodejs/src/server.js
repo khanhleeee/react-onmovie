69962a880664b30130e637e431a16b3b2b62b69d
Nguyenle23___ <beenbin2308@gmail.com>
2022-09-20 13:30:19 +0700

    Config port 3000 for NestJS and 4000 for NodeJS

--- server-nodejs/src/server.js
+++ server-nodejs/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5555 || process.env.PORT;
+const port = 4000 || process.env.PORT;
 const route = require('./routes');
 
 app.use(express.json());
