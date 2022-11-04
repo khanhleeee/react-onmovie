5e8b28c28effa111c49b01b6b16abad0cb682bb1
Nguyenle23___ <beenbin2308@gmail.com>
2022-11-04 15:46:30 +0700

    Nguyen | Refactor | Remove other servers

--- server-nestjs/src/modules/genre.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GenreController } from 'src/controllers/genre.controller';
-import { GenreService } from 'src/services/genre.service';
-
-@Module({
-  controllers: [GenreController],
-  providers: [GenreService]
-})
-export class GenreModule {}
