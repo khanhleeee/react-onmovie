96dc95c0db3f79e93fecc751f5e48f3f2b617a04
khanhleeee <khanhleee.itvn@gmail.con>
2022-11-24 16:24:09 +0700

    Kas | Modify | Constants | Add | Edit movie page

--- dashboard/src/routes/index.js
+++ dashboard/src/routes/index.js
@@ -1,4 +1,5 @@
 import AddMovie from "../pages/addMovie/AddMovie";
+import EditMovie from "../pages/editMovie/EditMovie";
 import Movie from "../pages/movie/Movie";
 import MovieList from "../pages/movieList/MovieList";
 import UserList from "../pages/userList/UserList";
@@ -7,6 +8,7 @@ import UserList from "../pages/userList/UserList";
 const publicRoutes = [
   { path: "/", component: MovieList },
   { path: "/movie/add", component: AddMovie },
+  { path: "/movie/edit/:id", component: EditMovie },
   { path: "/movie", component: Movie },
 ];
 
