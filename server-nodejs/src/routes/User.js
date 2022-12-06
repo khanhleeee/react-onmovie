fe1f5d71dc63774b42b4ab2fa11cde16710ca959
Nguyenle23___ <beenbin2308@gmail.com>
2022-12-06 11:54:33 +0700

    Nguyen | Chore | Change procedure register & login to create a new user in SQL Server

--- server-nodejs/src/routes/User.js
+++ server-nodejs/src/routes/User.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const userController = require('../controllers/UserController');
 
 router.get('/getWatchList/:userID', userController.getWatchList);
-router.post('/addWatchList', userController.addWatchList);
-router.post('/removeWatchList', userController.removeWatchList);
-
 router.get('/getRatingList/:userID', userController.getRatingList);
+
+router.post('/addWatchList', userController.addWatchList);
 router.post('/addRating', userController.addRating);
+router.post('/removeWatchList', userController.removeWatchList);
 
 module.exports = router;
